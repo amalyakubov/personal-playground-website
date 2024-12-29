c0419746a54c794feb8116993b8d07eed9a7b66f
Amal Yakubov <amal.yakubov7777@gmail.com>
2024-12-29 19:31:29 +0100

    Add padding to the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import { GlitchyTypewriter } from "./Typewriter";
 const Hero = () => {
   return (
     <>
-      <div className={`uppercase text-7xl font-bold leading-tighter pl-9 pb-9 font-['Space_Grotesk']`}>
+      <div className={`pt-16 uppercase text-7xl font-bold leading-tighter pl-9 pb-9 font-['Space_Grotesk']`}>
         <GlitchyTypewriter text={"AI-powered"} delay={50}></GlitchyTypewriter>
         <GlitchyTypewriter delay={50} className={"pl-14"} text={"SEO content agency"}></GlitchyTypewriter>
       </div>
