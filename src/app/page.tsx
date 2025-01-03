3f35947a0ce583146ed5a7aff24cc06faa767486
Amal Yakubov <amal.yakubov7777@gmail.com>
2025-01-03 12:01:20 +0100

    Fixed grid styilng

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,12 @@ const Home = () => {
   });
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
-      <div className={"page-container px-14 w-screen h-screen selection:bg-amber-300 font-['Geist']"} id="page-wrapper">
+      <div
+        className={
+          "page-container px-14 w-screen h-screen selection:bg-amber-300 font-['Geist']"
+        }
+        id="page-wrapper"
+      >
         <Header />
         <Hero />
         <div className={"grid grid-cols-2 w-screen gap-9 "}>
@@ -44,7 +49,11 @@ const Home = () => {
                 scale: 1.03,
               }}
             >
-              <Image src={heroBanner} alt={"asdasd"} className={"pt-16 size-12/12"} />
+              <Image
+                src={heroBanner}
+                alt={"asdasd"}
+                className={"pt-16 size-12/12"}
+              />
             </MotionDiv>
           </div>
           <MotionDiv
@@ -62,25 +71,35 @@ const Home = () => {
                 transition={{ duration: 0.3 }}
               />
               <p className={"pt-5 text-2xl leading-relaxed  pr-44 px-14"}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent imperdiet enim eget magna pretium
-                tristique. In tincidunt mi nec enim gravida, ut dictum arcu tincidunt. Praesent eget pretium massa, et
-                laoreet sem. Aliquam blandit imperdiet erat in finibus. Quisque iaculis libero nisl, elementum varius
-                lectus finibus vitae. Mauris nec ligula mollis, pharetra tellus sed, ornare purus. Nam quis ex vitae
-                nisi suscipit dictum et et lectus. Donec a tincidunt felis. Integer at pulvinar dolor. Pellentesque
-                habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque tristique,
-                diam id feugiat vestibulum, risus nisl maximus est, vel tincidunt risus ligula eget urna. Vivamus
-                maximus, felis sed tincidunt lobortis, velit purus congue arcu, sit amet faucibus urna massa et odio.{" "}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Praesent imperdiet enim eget magna pretium tristique. In
+                tincidunt mi nec enim gravida, ut dictum arcu tincidunt.
+                Praesent eget pretium massa, et laoreet sem. Aliquam blandit
+                imperdiet erat in finibus. Quisque iaculis libero nisl,
+                elementum varius lectus finibus vitae. Mauris nec ligula mollis,
+                pharetra tellus sed, ornare purus. Nam quis ex vitae nisi
+                suscipit dictum et et lectus. Donec a tincidunt felis. Integer
+                at pulvinar dolor. Pellentesque habitant morbi tristique
+                senectus et netus et malesuada fames ac turpis egestas.
+                Pellentesque tristique, diam id feugiat vestibulum, risus nisl
+                maximus est, vel tincidunt risus ligula eget urna. Vivamus
+                maximus, felis sed tincidunt lobortis, velit purus congue arcu,
+                sit amet faucibus urna massa et odio. <br />
                 <br />
-                <br />
-                Integer eget elit elit. Nullam sed tellus nisl. Nullam vel ornare dolor. Fusce eu pharetra metus. Etiam
-                vitae lorem elit. Aliquam nec consequat leo. Quisque ultricies leo a ipsum accumsan tempus. Nam non
-                bibendum ante. Integer sit amet tincidunt metus. Praesent dignissim, enim eu euismod tincidunt, elit
-                lacus rutrum libero, sit amet tincidunt ante nulla a felis. Vestibulum hendrerit urna lorem, nec aliquam
-                dolor porta ut. Phasellus rutrum nisl eu libero tincidunt, id eleifend massa feugiat. In eu purus dictum
-                nisi tincidunt laoreet nec vitae turpis. Mauris eu fringilla lectus, at feugiat mi. Praesent aliquam
-                maximus libero, fringilla suscipit mauris cursus eu. Nulla tortor augue, rutrum condimentum tortor
-                vitae, pellentesque congue risus. Cras ac arcu id lacus luctus ultrices. Curabitur ut sollicitudin
-                risus.
+                Integer eget elit elit. Nullam sed tellus nisl. Nullam vel
+                ornare dolor. Fusce eu pharetra metus. Etiam vitae lorem elit.
+                Aliquam nec consequat leo. Quisque ultricies leo a ipsum
+                accumsan tempus. Nam non bibendum ante. Integer sit amet
+                tincidunt metus. Praesent dignissim, enim eu euismod tincidunt,
+                elit lacus rutrum libero, sit amet tincidunt ante nulla a felis.
+                Vestibulum hendrerit urna lorem, nec aliquam dolor porta ut.
+                Phasellus rutrum nisl eu libero tincidunt, id eleifend massa
+                feugiat. In eu purus dictum nisi tincidunt laoreet nec vitae
+                turpis. Mauris eu fringilla lectus, at feugiat mi. Praesent
+                aliquam maximus libero, fringilla suscipit mauris cursus eu.
+                Nulla tortor augue, rutrum condimentum tortor vitae,
+                pellentesque congue risus. Cras ac arcu id lacus luctus
+                ultrices. Curabitur ut sollicitudin risus.
               </p>
             </div>
           </MotionDiv>
@@ -99,37 +118,60 @@ const GalleryOfImages = () => {
       </p>
       <div id={"flex-box-container"} className={"flex flex-row pb-20"}>
         <p className={"text-2xl w-[30vw] leading-relaxed"}>
-          We set sail on this new sea because there is new knowledge to be gained, and new rights to be won, and they
-          must be won and used for the progress of all people. For space science, like nuclear science and all
-          technology, has no conscience of its own. Whether it will become a force for good or ill depends on man, and
-          only if the United States occupies a position of pre-eminence can we help decide whether this new ocean will
-          be a sea of peace or a new terrifying theater of war. I do not say that we should or will go unprotected
-          against the hostile misuse of space any more than we go unprotected against the hostile use of land or sea,
-          but I do say that space can be explored and mastered without feeding the fires of war, without repeating the
-          mistakes that man has made in extending his writ around this globe of ours. There is no strife, no prejudice,
-          no national conflict in outer space as yet. Its hazards are hostile to us all. <br />
+          We set sail on this new sea because there is new knowledge to be
+          gained, and new rights to be won, and they must be won and used for
+          the progress of all people. For space science, like nuclear science
+          and all technology, has no conscience of its own. Whether it will
+          become a force for good or ill depends on man, and only if the United
+          States occupies a position of pre-eminence can we help decide whether
+          this new ocean will be a sea of peace or a new terrifying theater of
+          war. I do not say that we should or will go unprotected against the
+          hostile misuse of space any more than we go unprotected against the
+          hostile use of land or sea, but I do say that space can be explored
+          and mastered without feeding the fires of war, without repeating the
+          mistakes that man has made in extending his writ around this globe of
+          ours. There is no strife, no prejudice, no national conflict in outer
+          space as yet. Its hazards are hostile to us all. <br />
           <br />
-          Its conquest deserves the best of all mankind, and its opportunity for peaceful cooperation may never come
-          again. But why, some say, the Moon? Why choose this as our goal? And they may well ask, why climb the highest
-          mountain? Why, 35 years ago, fly the Atlantic? Why does Rice play Texas? We choose to go to the Moon. We
-          choose to go to the Moon... We choose to go to the Moon in this decade and do the other things, not because
-          they are easy, but because they are hard; because that goal will serve to organize and measure the best of our
-          energies and skills, because that challenge is one that we are willing to accept, one we are unwilling to
-          postpone, and one we intend to win, and the others, too.
+          Its conquest deserves the best of all mankind, and its opportunity for
+          peaceful cooperation may never come again. But why, some say, the
+          Moon? Why choose this as our goal? And they may well ask, why climb
+          the highest mountain? Why, 35 years ago, fly the Atlantic? Why does
+          Rice play Texas? We choose to go to the Moon. We choose to go to the
+          Moon... We choose to go to the Moon in this decade and do the other x
+          things, not because they are easy, but because they are hard; because
+          that goal will serve to organize and measure the best of our energies
+          and skills, because that challenge is one that we are willing to
+          accept, one we are unwilling to postpone, and one we intend to win,
+          and the others, too.
         </p>
 
-        <div className={"grid grid-cols-2 gap-5 pl-20  pr-20 text-2xl w-[80%]  "}>
-          <Image src={image1} alt={"image1"} className={"drop-shadow-2xl shadow-amber-800 col-start-1"} />
+        <div
+          className={"grid grid-cols-2 gap-5 pl-20  pr-20 text-2xl w-[80%]  "}
+        >
+          <Image
+            src={image1}
+            alt={"image1"}
+            className={"drop-shadow-2xl shadow-amber-800 col-start-1"}
+          />
           <Image
             src={image2}
             alt={"image2"}
-            className={"flex col-start-2 drop-shadow-2xl object-cover object-center h-full"}
+            className={
+              "flex col-start-2 drop-shadow-2xl object-cover object-center h-full"
+            }
           />
-          <Image src={image3} alt={"image3"} className={"col-start-1 drop-shadow-2xl row-start-1 "} />
+          <Image
+            src={image3}
+            alt={"image3"}
+            className={"col-start-1 drop-shadow-2xl row-start-1 "}
+          />
           <Image
             src={image4}
             alt={"image4"}
-            className={"col-start-2 row-start-1 drop-shadow-2xl h-full object-cover object-center "}
+            className={
+              "col-start-2 row-start-1 row-span-1 col-span-1 prow-start-1 drop-shadow-2xl h-full object-cover object-center "
+            }
           />
         </div>
       </div>
